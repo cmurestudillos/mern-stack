89c1e56fe3e80b5059827a066870814d44c6e40b
Carlos Mur <cmurdeveloper@gmail.com>
2023-12-07 20:18:45 +0100

    feat: Update server enviroment

--- client/src/components/shared/Footer.jsx
+++ client/src/components/shared/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return ( 
         <footer className="py-1 fixed-bottom bg-custom">
             <div className="container px-4 px-lg-5">
-                <p className="m-0 text-center text-white"> Copyright © - Designed and Created by: <a href="https://www.cmurestudillos.es" target="_blank" rel="noopener noreferrer" className="text-white"><strong>Carlos Mur</strong></a></p>
+                <p className="m-0 text-center text-white"> Copyright © - Designed and Created by: <strong>Carlos Mur</strong></p>
                 <p className="m-0 text-center text-white">
                     <small><i>Version: {packageInfo.version}</i></small>
                 </p>
